42aade3a0bc1f150c7ee0cf0d38a89ac8da3b5ee
kcz <kcz@kcz.sh>
2025-09-07 00:16:47 -0400

    close

--- rename.c
+++ rename.c
@@ -11,23 +11,23 @@ int rename(const char *oldpath, const char *newpath)
 
     int oldfd = open(oldpath, O_RDONLY);
     if (oldfd == -1) {
-        return -1;
+        goto err3;
     }
     int newfd = open(newpath, O_WRONLY | O_CREAT | O_TRUNC);
     if (newfd == -1) {
-        return -1;
+        goto err2;
     }
 
     struct stat st;
     if (fstat(oldfd, &st) == -1) {
-        return -1;
+        goto err1;
     }
 
     size_t sz = st.st_size;
 
     int ret = copy_file_range(oldfd, NULL, newfd, NULL, sz, 0);
     if (ret == -1) {
-        return -1;
+        goto err1;
     }
 
     dprintf(2, "rename: copied %s -> %s (%d bytes)\n", oldpath, newpath, ret);
@@ -35,4 +35,11 @@ int rename(const char *oldpath, const char *newpath)
     unlink(oldpath);
 
     return 0;
+
+err1:
+    close(newfd);
+err2:
+    close(oldfd);
+err3:
+    return -1;
 }
